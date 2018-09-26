928a45ae726f355ab0be738aef930b575f86745c
John Francis dela Vega <developer@francismedia.net>
2018-09-26 18:34:49 +0800

    calculator

--- src/app.js
+++ src/app.js
@@ -1,1 +1,57 @@
 import dom from "ampersand-dom";
+
+export default class Calculator {
+    constructor() {
+        this.numberValue = 0;
+        this.savedValue = 0;
+    }
+
+    operate (operator) {
+        this.operator = operator;
+        this.savedValue = this.numberValue;
+        this.endofcalc = true;
+    }
+
+    allClear() {
+        this.numberValue = 0;
+    }
+
+    percent() {
+        this.numberValue = (parseFloat(this.numberValue) * 0.01);
+    }
+
+    plusminus() {
+        this.numberValue = this.numberValue > 0 ? -Math.abs(this.numberValue) : Math.abs(this.numberValue);
+    }
+
+    process(value) {
+        if (value === '●') value = '.';
+        this.numberValue = this.numberValue && !this.endofcalc ? this.numberValue + value : value;
+        this.endofcalc = false;
+    }
+
+    prepend(value) {
+        this.process(value);
+    }
+
+    equals() {
+        switch (this.operator) {
+            case 'sum':
+                this.numberValue = parseFloat(this.savedValue) + parseFloat(this.numberValue);
+                this.endofcalc = true;
+                break;
+            case 'sub':
+                this.numberValue = parseFloat(this.savedValue) - parseFloat(this.numberValue);
+                this.endofcalc = true;
+                break;
+            case 'mul':
+                this.numberValue = parseFloat(this.savedValue) * parseFloat(this.numberValue);
+                this.endofcalc = true;
+                break;
+            case 'div':
+                this.numberValue = parseFloat(this.savedValue) / parseFloat(this.numberValue);
+                this.endofcalc = true;
+                break;
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,60 @@
 import "./index.css";
-import "./app.js";
+import Calculator from "./app.js";
 import template from "./template";
 
 document.getElementById("app").innerHTML = template;
+var calculator = new Calculator();
+document
+    .querySelectorAll('.digit-keys *')
+    .forEach(function (item) {
+        item.addEventListener('click', function () {
+            calculator.prepend(this.innerHTML);
+        });
+    });
+
+document
+    .querySelectorAll('.function-keys *')
+    .forEach(function (item) {
+        item.addEventListener('click', function () {
+            switch (this.innerHTML) {
+                case 'AC':
+                    calculator.allClear();
+                    break;
+                case '±':
+                    calculator.plusminus();
+                    break;
+                case '%':
+                    calculator.percent();
+                    break;
+            }
+        });
+    });
+
+document
+    .querySelectorAll('.operator-keys *')
+    .forEach(function (item) {
+        item.addEventListener('click', function () {
+            switch (this.innerHTML) {
+                case '÷':
+                    calculator.operate('sum');
+                    break;
+                case '×':
+                    calculator.operate('mul');
+                    break;
+                case '−':
+                    calculator.operate('sub');
+                    break;
+                case '+':
+                    calculator.operate('sum');
+                    break;
+                default:
+                    calculator.equals();
+            }
+        });
+    });
+
+document
+    .querySelector('.calculator')
+    .addEventListener('click', function () {
+        document.querySelector('.auto-scaling-text').innerHTML = calculator.numberValue;
+    });
